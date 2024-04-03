e5aa323445293d4aa2fbf23c35e526ecbc6b84e2
Anton Stavnichiy <anton.stavnichiy@gmail.com>
2024-04-03 17:49:55 +0600

    Add static address

--- Sources/BinanceChainKit/Core/BinanceChainKit.swift
+++ Sources/BinanceChainKit/Core/BinanceChainKit.swift
@@ -307,3 +307,12 @@ extension BinanceChainKit {
     }
 
 }
+
+extension BinanceChainKit {
+    public static func firstAddress(seed: Data, networkType: NetworkType) throws -> String {
+        let helper = segWitHelper(networkType: networkType)
+        let wallet = try wallet(seed: seed, segWitHelper: helper)
+        
+        return wallet.address
+    }
+}
